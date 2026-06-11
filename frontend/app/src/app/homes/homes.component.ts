d47e5e8fe42f3526d968fb75cc8f8e603e7d4054
HR18HR <hamza.rofi@gmail.com>
2026-06-11 19:12:53 +0200

    aggiunto

--- frontend/app/src/app/homes/homes.component.ts
+++ frontend/app/src/app/homes/homes.component.ts
@@ -1,9 +1,9 @@
 import { NgFor, NgIf } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component,OnInit,AfterViewInit } from '@angular/core';
 import { FormsModule, NgModel } from '@angular/forms';
 import { Token, UserService } from '../user.service';
 import { jwtDecode } from 'jwt-decode';
-
+import { Esami, EsamiPratica, Istituti, PraticheService ,Pratica} from '../pratiche.service';
 
 @Component({
   selector: 'app-home',
@@ -17,25 +17,142 @@ to:Token={id:0,nome:"",cognome:"",email:"",ruolo:"",data_nascita:null,password:"
 risposta:string=''; //mostar i messaggi ricevuti dal backend
 res:{Pos:number,Neg:number}={Pos:0,Neg:0} //oggetto che mostra il div della risposta backend
 message:string=" "
-mostraModifica: boolean = false;
+ mostraModifica: boolean = false;
+
+ istituti:Istituti[]=[]
+ esami :Esami[]=[]
+
+ pratica = {
+  studente_email: '',
+  docente_email: null as string | null,
+  data_inizio: '',
+  data_fine: null as string | null,
+  nome_istituto: ''
+};
+mostraPratica: boolean = false;
+mostraEsami: boolean = false;
+
+esamiPratica: EsamiPratica[] = [];
+
+idPratica: string = '';
+emailStudente: string = '';
+emailDocente: string = '';
+ filePratica: File | null = null;
+
+dataPartenza: string = '';
+
+dataRientro: string = '';
+
+semestre: string = '';
+
+nomeIstituto: string = '';
+
+pratiche_1: Pratica[] = [];
 
 
-constructor( public user:UserService) {}
+constructor( public user:UserService,public pratiche:PraticheService) {}
 
 
- ngAfterViewInit() {
+ ngOnInit() {
     const token = localStorage.getItem('jwt');
 
     if (token != null) {
       this.to= jwtDecode<Token>(token);
+      this.pratica.studente_email=this.to.email
+      this.pratiche.GetIstituti(token).subscribe({
+        next:(r)=>{
+          this.istituti=r
+          alert(this.istituti[0].nome)
 
-    }
+        },
+        error:(err)=>{
+        
+        }
+      })
+
+
+      this.pratiche.GetEsami(token).subscribe({
+        next:(r)=>{
+          this.esami=r
+          alert(this.esami[0].nome)
+
+        },
+        error:(err)=>{
+        
+        }
+      })
+    
+    this.pratiche.GetPraticheUtente(token)
+
+    .subscribe({
+
+      next: (response) => {
+
+        this.pratiche_1 = response;
+
+
+      },
+
+      error: (err) => {
+
+
+      }
+
+    });
+
+
+
+    
   }
 
-  
-  VisualizzaModifica() {
-    !this.mostraModifica ;
+
+
   }
+
+
+   VisualizzaModifica() {
+
+    this.mostraModifica = !this.mostraModifica;
+  }
+
+  VisualizzaEsami() {
+  this.mostraEsami = !this.mostraEsami;
+}
+
+
+VisualizzaPratica() {
+  if (!this.mostraPratica) {
+    this.mostraPratica = true;
+
+    this.idPratica = this.generaIdPratica();
+    this.emailStudente = this.to.email;
+
+    this.emailDocente = '';
+    this.nomeIstituto = '';
+    this.dataPartenza = '';
+    this.dataRientro = ' ';
+    this.semestre = '';
+
+    this.esamiPratica = [];
+    this.filePratica = null;
+  } else {
+    this.chiudiPratica();
+  }
+}
+
+chiudiPratica() {
+  this.mostraPratica = false;
+
+  this.idPratica = '';
+  this.emailStudente = '';
+  this.emailDocente = '';
+  this.esamiPratica = [];
+  this.filePratica = null;
+  this.dataPartenza=' ';
+  this.semestre=' ';
+  this.dataRientro='';
+  this.nomeIstituto='';
+}
 
 
   aggiornaUtente() {
@@ -77,6 +194,113 @@ constructor( public user:UserService) {}
 });
 
 }
+
+
+
+
+
+generaIdPratica(lunghezza: number = 12): string {
+  const caratteri = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let risultato = '';
+
+  for (let i = 0; i < lunghezza; i++) {
+    risultato += caratteri.charAt(Math.floor(Math.random() * caratteri.length));
+  }
+
+  return risultato;
+}
+
+aggiungiRigaEsame() {
+
+  this.esamiPratica.push({
+    id: this.esamiPratica.length + 1,
+    pratica_id: this.idPratica,
+    esame_locale_nome: '',
+    esame_estero_nome: ''
+  });
+
+}
+
+rimuoviEsamePratica(index: number) {
+
+  this.esamiPratica.splice(index, 1);
+
+}
+
+selezionaFile(event: Event) {
+
+  const input = event.target as HTMLInputElement;
+
+  if (input.files && input.files.length > 0) {
+    this.filePratica = input.files[0];
+  }
+
+}
+
+
+
+
+creaPratica() {
+  const token = localStorage.getItem("jwt");
+
+  if (!token) {
+    this.message = "Token mancante";
+    return;
+  }
+
+  this.pratiche.CreaPratica(
+    token,
+    this.idPratica,
+    this.emailStudente,
+    this.emailDocente,
+    this.nomeIstituto,
+    this.dataPartenza,
+    this.dataRientro,
+    this.semestre,
+    this.esamiPratica,
+    this.filePratica
+  ).subscribe({
+    next: (r) => {
+      this.message = r.message;
+      this.res.Pos = 1;
+      this.res.Neg = 0;
+
+      this.chiudiPratica();
+    },
+    error: (err) => {
+      this.message = err.error?.errore ?? "Errore creazione pratica";
+      this.res.Pos = 0;
+      this.res.Neg = 1;
+    }
+  });
+
+  setTimeout(()=>{
+    this.pratiche.GetPraticheUtente(token)
+
+    .subscribe({
+
+      next: (response) => {
+
+        this.pratiche_1 = response;
+
+
+      },
+
+      error: (err) => {
+
+
+      }
+
+    });
+
+  
+
+},2000)
+
+
+
+}
+
 
 
 
---
+++ frontend/app/src/app/pratiche.service.ts
@@ -0,0 +1,164 @@
+import { Injectable } from '@angular/core';
+import { HttpClient,HttpHeaders,provideHttpClient} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Buffer } from 'buffer';
+
+export interface Istituti{
+  nome:string,
+  paese:string,
+  citta:string,
+  indirizzo:string
+}
+
+export interface Utenti {
+  id: number;
+  nome: string;
+  cognome: string;
+  email: string;
+  password_hash: string;
+  salt: string;
+  ruolo: 'S' | 'D' | 'U';
+  data_nascita: string; // DATE → string (ISO format)
+}
+
+export interface Istituti {
+  nome: string;
+  indirizzo: string;
+  citta: string ;
+  paese: string;
+}
+
+export interface Pratica {
+  id: string; // TEXT, non number
+  studente_email: string;
+  docente_email: string | null;
+  nome_istituto: string | null;
+  stato:
+    | 'CREATA'
+    | 'ATT_APPROVAZIONE'
+    | 'APPROVATA_DOCENTE'
+    | 'APPROVATA_UFFICIO'
+    | 'MOBILITA_IN_CORSO'
+    | 'APPROVATO_TRANSCRIPT'
+    | 'CHIUSA';
+  data_creazione: string; // TIMESTAMP → string
+  motivazione: string | null;
+  data_inizio: string;   // DATE → string
+  data_fine: string | null;
+  esami:EsamiPratica[]
+}
+
+export interface EsamiEsteri {
+  id: string;
+  nome: string;
+  crediti: number;
+}
+
+export interface Esami {
+  nome: string;
+  crediti: number;
+  nome_esame_estero: string | null;
+}
+
+export interface EsamiPratica {
+  id: number;
+  pratica_id: string;
+  esame_locale_nome: string;
+  esame_estero_nome: string;
+}
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PraticheService {
+
+  constructor(public Http: HttpClient) { }
+
+
+
+
+
+
+
+
+GetIstituti(token: string): Observable<Istituti[]> {
+  const headers = new HttpHeaders({
+    'Authorization': 'Bearer ' + token
+  });
+  
+  return this.Http.get<Istituti[]>('http://localhost:5000/istituti', { headers });
+}
+
+GetEsami(token: string): Observable<Esami[]> {
+  const headers = new HttpHeaders({
+    'Authorization': 'Bearer ' + token
+  });
+  
+  return this.Http.get<Esami[]>('http://localhost:5000/esami', { headers });
+}
+
+
+
+
+
+CreaPratica(
+  token: string,
+  idPratica: string,
+  emailStudente: string,
+  emailDocente: string,
+  nomeIstituto: string,
+  dataPartenza: string,
+  dataRientro: string | null,
+  semestre: string,
+  esami: EsamiPratica[],
+  file: File | null
+):Observable<{message:string}> {
+  const formData = new FormData();
+
+  formData.append("id_pratica", idPratica);
+  formData.append("email_studente", emailStudente);
+  formData.append("email_docente", emailDocente);
+  formData.append("nome_istituto", nomeIstituto);
+  formData.append("data_partenza", dataPartenza);
+  formData.append("data_rientro", dataRientro ?? "");
+  formData.append("semestre", semestre);
+  formData.append("esami", JSON.stringify(esami));
+
+  if (file) {
+    formData.append("agreement", file);
+  }
+
+  return this.Http.post<{message:string}>(
+    'http://localhost:5000/pratica',
+    formData,
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
+}
+
+
+
+
+GetPraticheUtente(
+  token: string
+): Observable<Pratica[]> {
+
+  return this.Http.get<Pratica[]>(
+    'http://localhost:5000/pratiche',
+    {
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    }
+  );
+}
+
+
+
+
+}
